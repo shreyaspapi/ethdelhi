da49ef42324bb4c10299499fcd43a25b95c0258d
Souvik Mishra <mishra.souvik911@gmail.com>
2025-09-27 19:25:56 +0530

    add theming

---
+++ interface/src/components/theme-toggle.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import * as React from 'react';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useAppKitTheme } from '@reown/appkit/react';
+
+export function ThemeToggle() {
+  const { setTheme } = useTheme();
+  const { setThemeMode } = useAppKitTheme();
+
+  const handleThemeToggle = (theme: string) => {
+    setTheme(theme);
+    setThemeMode(theme as 'light' | 'dark');
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => handleThemeToggle('light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleThemeToggle('dark')}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleThemeToggle('system')}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
